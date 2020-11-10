acce07f8430e2540c8e78d5dc40da7239940631b
alisonhussey <alisonhussey@gmail.com>
2020-11-10 02:28:01 +0000

    updated file

--- hello.js
+++ hello.js
@@ -2,3 +2,11 @@ const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("Alison");
+
+const returnSayHello = function (name) {
+  return "hello, " + name;
+
+}
+const greeting = returnSayHello("Alison");
+ console.log(greeting);
+ 
